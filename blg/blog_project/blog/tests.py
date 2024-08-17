974baa648b0b3ca27eb526f352222cbb0fd32f1d
SakuraSher <sakurasherlocked@gmail.com>
2024-08-17 22:13:26 +0530

    blog updates

--- blg/blog_project/blog/tests.py
+++ blg/blog_project/blog/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
+from .models import Post
 # Create your tests here.
+
+class BlogTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email= 'test@email.com',
+            password='secret',
+        )
+        self.post = Post.objects.create(
+            title = "A",
+            body = "B",
+            author = self.user,
+        )
+
+    
+    def test_string_representation(self):
+        post = Post(title="A")
+        self.assertEqual(str(post),post.title)
